a778315c9d4577aa1cb0d227a5479b6c9af2ba1a
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-06-11 04:57:28 -0600

    remove whitespaces.

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,4 +4,26 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
+
+  def rank_of_card_at(i)
+    @cards[i].rank
+  end
+
+  def high_ranking_cards
+    @cards.select do |card|
+      card.rank >= 11
+    end
+  end
+
+  def percent_high_ranking
+   raw_percent_high_rankings = ((high_ranking_cards.size.to_f / @cards.size.to_f) * 100).round(2)
+  end
+
+  def remove_card
+    @cards.shift
+  end
+
+  def add_card(card)
+    @cards.push(card)
+  end
 end
